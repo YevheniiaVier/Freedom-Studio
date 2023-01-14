222739b66011358c600947ecb82d89c007b5dc0b
YevheniiaVier <gotashk1983@gmail.com>
2023-01-14 21:23:36 +0200

    adds main files

---
+++ src/js/language-menu.js
@@ -0,0 +1,18 @@
+(() => {
+  const mobileMenu = document.querySelector('.js-lang-menu-container');
+  const openMenuBtn = document.querySelector('.js-open-lang-menu');
+
+  const toggleMenu = () => {
+    const isMenuOpen =
+      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+    mobileMenu.classList.toggle('is-open');
+
+    const scrollLockMethod = !isMenuOpen
+      ? 'disableBodyScroll'
+      : 'enableBodyScroll';
+    bodyScrollLock[scrollLockMethod](document.body);
+  };
+
+  openMenuBtn.addEventListener('click', toggleMenu);
+})();
